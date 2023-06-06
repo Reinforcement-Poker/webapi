1144578fca33e85c84bad7e5670046a793f9a9d9
Asier Villar <asiervillar@outlook.es>
2023-06-07 01:56:29 +0200

    ADD: main.py

---
+++ src/main.py
@@ -0,0 +1,33 @@
+from pprint import pprint
+
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+from replaypoker import ReplayPoker
+from utils.settings import TARGET_PSW, TARGET_URL, TARGET_USR
+
+if __name__ == "__main__":
+    driver = webdriver.Remote(
+        command_executor="http://localhost:4444",
+        desired_capabilities=DesiredCapabilities.FIREFOX,
+    )
+    driver.implicitly_wait(10)
+
+    try:
+        replaypoker = ReplayPoker(driver)
+
+        # Pipeline for playing
+        replaypoker.login(TARGET_USR, TARGET_PSW)
+        lobbies = replaypoker.get_all_lobbies()
+        pprint(lobbies)
+
+        for lobby in lobbies:
+            if not lobby.is_full:
+                replaypoker.join_lobby(driver, lobby.link)
+                replaypoker.print_board_status()
+                break
+
+    except Exception as e:
+        print("This has bean failed", e)
+    finally:
+        driver.quit()
