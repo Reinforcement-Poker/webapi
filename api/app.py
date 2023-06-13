b160d7f08127c0ee8b936c73598dba364932dada
Asier Villar <asiervillar@outlook.es>
2023-06-13 23:50:59 +0200

    FIX: cards now parse correctly

--- api/app.py
+++ api/app.py
@@ -9,8 +9,8 @@ fuzzy_model = FuzzyModel()
 
 class GameState(BaseModel):
     legal_actions: list[str] = ["fold", "call", "raise_low", "raise_high", "all_in"]
-    player_cards: list[str] = ["dT", "dA"]
-    public_cards: list[str] = ["dK", "dQ", "dJ", "d9", "d8"]
+    player_cards: list[str] = ["Td", "Ad"]
+    public_cards: list[str] = ["Kd", "Qd", "Jd", "9d", "8d"]
     pot: int = 100
     cost: int = 20
     button: int = 1
@@ -19,8 +19,8 @@ class GameState(BaseModel):
 @app.post("/fuzzy_model")
 def fuzzy_predict(state: GameState) -> int:
     hand_score = fuzzy_model.get_hand_score(
-        state.player_cards,
-        state.public_cards,
+        [f"{card[1]}{card[0]}" for card in state.player_cards],
+        [f"{card[1]}{card[0]}" for card in state.public_cards],
     )
 
     score = fuzzy_model.make_prediction(
