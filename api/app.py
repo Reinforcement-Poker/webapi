6f8fcc08ba283dc3e6ce8abe0669f6afe4c9232b
Asier Villar <asiervillar@outlook.es>
2023-06-23 08:02:15 +0200

    ADD: new models and endpoints

--- api/app.py
+++ api/app.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
-from .models.fuzzy_model import FuzzyModel
+from api.database.core import engine, get_db
+from api.database.models import Base, Bet, Game, Player, State
+from api.models.fuzzy_model import FuzzyModel
 
 app = FastAPI()
 fuzzy_model = FuzzyModel()
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
 
 
 class GameState(BaseModel):
@@ -35,3 +40,28 @@ def fuzzy_predict(state: GameState) -> int:
         action_index = 1
 
     return action_index
+
+
+@app.post("/use_database")
+async def use_database(db: Session = Depends(get_db)) -> bool:
+    player1 = Player(username="John")
+    player2 = Player(username="Emma")
+    player3 = Player(username="Michael")
+    game = Game(n_players=3)
+    db.add_all([player1, player2, player3, game])
+    db.commit()
+
+    state1 = State(pot=1000, public_cards="AH KH QH", stage="preflop", game_id=game.id)
+    state2 = State(pot=500, public_cards="2C 3C 4C", stage="turn", game_id=game.id)
+    db.add_all([state1, state2])
+    db.commit()
+
+    bet1 = Bet(chips=200, action="fold", player_id=player1.id, state_id=state1.id)
+    bet2 = Bet(chips=200, action="fold", player_id=player2.id, state_id=state1.id)
+    bet3 = Bet(chips=200, action="fold", player_id=player3.id, state_id=state2.id)
+    bet4 = Bet(chips=200, action="fold", player_id=player1.id, state_id=state2.id)
+
+    db.add_all([bet1, bet2, bet3, bet4])
+    db.commit()
+
+    return True
--- api/database/models.py
+++ api/database/models.py
@@ -11,12 +11,44 @@ class Player(Base):
 
     id: Mapped[int] = MappedColumn(Integer, primary_key=True, nullable=False)
     username: Mapped[str] = MappedColumn(String(30), nullable=False)
-    player_id: Mapped[int]
 
 
 class State(Base):
     __tablename__ = "state"
 
     id: Mapped[int] = MappedColumn(Integer, primary_key=True, nullable=False)
+    game_id: Mapped[int] = MappedColumn(Integer, nullable=False)
+    stage: Mapped[str] = MappedColumn(String(30), nullable=False)
     pot: Mapped[int] = MappedColumn(Integer, nullable=False)
     public_cards: Mapped[str] = MappedColumn(String(30), nullable=False)
+
+
+class Game(Base):
+    __tablename__ = "game"
+
+    id: Mapped[int] = MappedColumn(Integer, primary_key=True, nullable=False)
+    n_players: Mapped[int] = MappedColumn(Integer, nullable=False)
+    states: Mapped[list["State"]] = relationship(
+        "State",
+        primaryjoin="foreign(Game.id) == State.game_id",
+        backref="game",
+    )
+
+
+class Bet(Base):
+    __tablename__ = "bet"
+
+    state_id: Mapped[int] = MappedColumn(Integer, primary_key=True, nullable=False)
+    player_id: Mapped[int] = MappedColumn(Integer, primary_key=True, nullable=False)
+
+    action: Mapped[str] = MappedColumn(String(30), nullable=False)
+    chips: Mapped[int] = MappedColumn(Integer, nullable=False)
+
+    players: Mapped[list["Player"]] = relationship(
+        "Player",
+        primaryjoin="foreign(Bet.player_id) == Player.id",
+    )
+    states: Mapped[list["State"]] = relationship(
+        "State",
+        primaryjoin="foreign(Bet.state_id) == State.id",
+    )
